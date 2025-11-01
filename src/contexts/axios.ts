30ac954784b0bdf53a23039ce6ed82278071b894
manoj-10705 <ganymede323@gmail.com>
2025-11-01 12:37:20 +0530

    Configured API base URL and a log for verify 2

--- src/contexts/axios.ts
+++ src/contexts/axios.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 console.log("🌐 Base URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ,
+  baseURL: import.meta.env.VITE_API_URL || "https://bookverse-1-tp4m.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 export default api;
